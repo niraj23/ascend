d475b3c1cc29d10dc1566c7656aa7fb2bfccb08c
Niraj Patel <niraj2323@gmail.com>
2021-07-29 22:58:13 -0400

    commit on 'master'

---
+++ Desktop/Portfolio-master/src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple"> Niraj Patel </spanr
+            from <span className="purple"> New Jersey, USA.</span>
+            <br />BLANK FOR NOW
+            <br />
+            <br />
+            BLANK FOR NOW
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> BLANK FOR NOW
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> BLANK FOR NOW
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> BLANK FOR NOW
+            </li>
+          </ul>
+
+          <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
+            "BLANK FOR NOW"{" "}
+          </p>
+          <footer className="blockquote-footer">BLANK FOR NOW</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
